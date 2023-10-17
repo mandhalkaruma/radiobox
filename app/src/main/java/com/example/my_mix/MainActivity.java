6e111b79f9e814426cb77e836fd154d2a902c6e2
JagdishMandhalkar <jagdishmandhalkar1508@gmail.com>
2023-10-17 23:44:33 +0530

    radiobtn

---
+++ app/src/main/java/com/example/my_mix/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.my_mix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    RadioGroup radio;
+    RadioButton radioButton;
+    Button submit;
+    TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        radio = findViewById(R.id.radio);
+        submit = findViewById(R.id.submit);
+        txt = findViewById(R.id.txt);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int ID = radio.getCheckedRadioButtonId();
+                radioButton = findViewById(ID);
+                txt.setText("Selected "+ radioButton.getText());
+            }
+        });
+    }
+}
